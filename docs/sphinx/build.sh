ceb6f5cfb3ec0d68337ad2e73d7df45ee7738b02
Simon Gravelle <simon.gravelle@live.fr>
2023-04-03 22:26:32 +0200

    added better build script

--- docs/sphinx/build.sh
+++ docs/sphinx/build.sh
@@ -1,9 +1,9 @@
 make clean
 make html
-cp source/index_replace.html build/html/index.html
-cp -r source/redirecting/2Dmaterials/ build/html/tutorials/
-cp -r source/redirecting/bulkfluids/ build/html/tutorials
-cp -r source/redirecting/freeenergy/ build/html/tutorials/
-cp -r source/redirecting/montecarlo/ build/html/tutorials/
-cp -r source/redirecting/reaxff/ build/html/tutorials/
+cp ./source/index_replace.html ./build/html/index.html
+cp -rf ./source/redirecting/2Dmaterials/ ./build/html/tutorials/
+cp -rf ./source/redirecting/bulkfluids/ ./build/html/tutorials
+cp -rf ./source/redirecting/freeenergy/ ./build/html/tutorials/
+cp -rf ./source/redirecting/montecarlo/ ./build/html/tutorials/
+cp -rf ./source/redirecting/reaxff/ ./build/html/tutorials/
 
