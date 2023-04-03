66d0dae8e3ea38cd7a3329ed4adfb8ea24b992f0
simongravelle <simongravelle@users.noreply.github.com>
2023-04-03 20:21:29 +0000

    deploy: d66438cefea86fc3c85812c1b3eb9e8469cc9323

--- sphinx/build.sh
+++ sphinx/build.sh
@@ -1,4 +1,9 @@
 make clean
 make html
 cp source/index_replace.html build/html/index.html
-cp -r source/redirecting/* build/html/tutorials/
+cp -r source/redirecting/2Dmaterials/ build/html/tutorials/
+cp -r source/redirecting/bulkfluids/ build/html/tutorials
+cp -r source/redirecting/freeenergy/ build/html/tutorials/
+cp -r source/redirecting/montecarlo/ build/html/tutorials/
+cp -r source/redirecting/reaxff/ build/html/tutorials/
+
