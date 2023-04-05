bf8b57fe2cae179f36e657ea9335ef68c4a9c47d
Simon Gravelle <simon.gravelle@live.fr>
2023-04-05 08:56:03 +0200

    removed redirecting file

--- docs/sphinx/build.sh
+++ docs/sphinx/build.sh
@@ -1,9 +1,3 @@
 make clean
 make html
 cp ./source/index_replace.html ./build/html/index.html
-cp -rf ./source/redirecting/2Dmaterials/ ./build/html/tutorials/
-cp -rf ./source/redirecting/bulkfluids/ ./build/html/tutorials
-cp -rf ./source/redirecting/freeenergy/ ./build/html/tutorials/
-cp -rf ./source/redirecting/montecarlo/ ./build/html/tutorials/
-cp -rf ./source/redirecting/reaxff/ ./build/html/tutorials/
-
