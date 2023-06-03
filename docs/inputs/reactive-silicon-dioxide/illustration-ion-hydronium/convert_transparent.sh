eef39acfe76ab04d28b8ec3e6cc3ca81cbef3535
Simon Gravelle <simon.gravelle@live.fr>
2023-06-03 13:00:29 +0200

    Added O2 molecules

--- docs/inputs/reactive-silicon-dioxide/illustration-ion-hydronium/convert_transparent.sh
+++ docs/inputs/reactive-silicon-dioxide/illustration-ion-hydronium/convert_transparent.sh
@@ -7,14 +7,14 @@ set -e
 for file in _light.*.ppm; 
 do 
 	echo ${file:0:12}.png
-	convert $file -transparent white ${file:0:12}.png;
+	convert $file -resize 421x221 -transparent white ${file:0:12}.png;
 done
-img2webp -o ../../../../docs/sphinx/source/tutorials/figures/reactive-silicon-dioxide/hydronium_transfert_light.webp -q 40 -mixed -d 22.22 _light*.png
+img2webp -o ../../../../docs/sphinx/source/tutorials/figures/reactive-silicon-dioxide/hydronium_transfert_light.webp -q 30 -mixed -d 11.11 _light*.png
 
 # 2) Generate black background movie
 for file in _dark.*.ppm; 
 do 
 	echo ${file:0:11}.png
-	convert $file -transparent black ${file:0:11}.png;
+	convert $file -resize 421x221 -resize  -transparent black ${file:0:11}.png;
 done
-img2webp -o ../../../../docs/sphinx/source/tutorials/figures/reactive-silicon-dioxide/hydronium_transfert_dark.webp -q 40 -mixed -d 22.22 _dark*.png
+img2webp -o ../../../../docs/sphinx/source/tutorials/figures/reactive-silicon-dioxide/hydronium_transfert_dark.webp -q 30 -mixed -d 11.11 _dark*.png
