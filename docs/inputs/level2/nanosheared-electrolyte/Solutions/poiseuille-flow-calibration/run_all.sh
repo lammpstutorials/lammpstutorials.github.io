62732e1cba135a0cbe6dfa68e7e27b5a1d85f4fc
Simon Gravelle <simon.gravelle@live.fr>
2023-12-04 18:42:49 +0100

    changed duration of data

---
+++ docs/inputs/level2/nanosheared-electrolyte/Solutions/poiseuille-flow-calibration/run_all.sh
@@ -0,0 +1,31 @@
+cd force_0.01/
+    mpirun -np 8 /home/simon/Softwares/lammps-2Aug2023/src/lmp_mpi -in input.lammps
+cd ..
+
+cd force_0.02/
+    mpirun -np 8 /home/simon/Softwares/lammps-2Aug2023/src/lmp_mpi -in input.lammps
+cd ..
+
+cd force_0.03/
+    mpirun -np 8 /home/simon/Softwares/lammps-2Aug2023/src/lmp_mpi -in input.lammps
+cd ..
+
+cd force_0.04/
+    mpirun -np 8 /home/simon/Softwares/lammps-2Aug2023/src/lmp_mpi -in input.lammps
+cd ..
+
+cd force_0.05/
+    mpirun -np 8 /home/simon/Softwares/lammps-2Aug2023/src/lmp_mpi -in input.lammps
+cd ..
+
+cd force_0.06/
+    mpirun -np 8 /home/simon/Softwares/lammps-2Aug2023/src/lmp_mpi -in input.lammps
+cd ..
+
+cd force_0.07/
+    mpirun -np 8 /home/simon/Softwares/lammps-2Aug2023/src/lmp_mpi -in input.lammps
+cd ..
+
+cd force_0.08/
+    mpirun -np 8 /home/simon/Softwares/lammps-2Aug2023/src/lmp_mpi -in input.lammps
+cd ..
