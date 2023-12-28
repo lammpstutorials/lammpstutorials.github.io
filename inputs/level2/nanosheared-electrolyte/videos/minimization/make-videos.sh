bf8df18e9ec7fd173391348e4befaf1f658d6697
simongravelle <simongravelle@users.noreply.github.com>
2023-12-28 16:33:20 +0000

    deploy: bbc5cdf26cd1dbf4fc348336395c95f7283f4d9e

---
+++ inputs/level2/nanosheared-electrolyte/videos/minimization/make-videos.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+
+set -e
+
+for file in untitled.*.ppm; do convert $file -crop 1504x786+0+0 $file; done
+ffmpeg -r 30 -i untitled.%05d.ppm -vcodec libx264 -crf 0  -pix_fmt yuv420p myvideo.mp4
