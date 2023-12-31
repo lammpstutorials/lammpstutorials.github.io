8411bd0c07631e1f77587fe63b32abb486a1d8b3
Simon Gravelle <simon.gravelle@live.fr>
2023-12-31 16:13:18 +0100

    chaged calibration data

--- docs/inputs/level2/nanosheared-electrolyte/solutions/poiseuille-flow-calibration/run_all.sh
+++ docs/inputs/level2/nanosheared-electrolyte/solutions/poiseuille-flow-calibration/run_all.sh
@@ -1,5 +1,13 @@
 lmp=/work/sgravelle/Softwares/lammps-3Nov2022/src/lmp_mpi
 
+cd force_0.0025/
+    mpirun -np 8 ${lmp} -in input.lammps
+cd ..
+
+cd force_0.005/
+    mpirun -np 8 ${lmp} -in input.lammps
+cd ..
+
 cd force_0.01/
     mpirun -np 8 ${lmp} -in input.lammps
 cd ..
@@ -8,26 +16,7 @@ cd force_0.02/
     mpirun -np 8 ${lmp} -in input.lammps
 cd ..
 
-cd force_0.03/
-    mpirun -np 8 ${lmp} -in input.lammps
-cd ..
-
 cd force_0.04/
     mpirun -np 8 ${lmp} -in input.lammps
 cd ..
 
-cd force_0.05/
-    mpirun -np 8 ${lmp} -in input.lammps
-cd ..
-
-cd force_0.06/
-    mpirun -np 8 ${lmp} -in input.lammps
-cd ..
-
-cd force_0.07/
-    mpirun -np 8 ${lmp} -in input.lammps
-cd ..
-
-cd force_0.08/
-    mpirun -np 8 ${lmp} -in input.lammps
-cd ..
