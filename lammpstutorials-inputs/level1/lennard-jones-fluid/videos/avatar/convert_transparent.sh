10b743a4a07f061498d1d5da3edb695a186b9d34
simongravelle <simongravelle@users.noreply.github.com>
2024-02-16 21:05:40 +0000

    deploy: 287986ef64aedb870e80f793f0aa6cfdc75d9460

--- lammpstutorials-inputs/level1/lennard-jones-fluid/videos/avatar/convert_transparent.sh
+++ lammpstutorials-inputs/level1/lennard-jones-fluid/videos/avatar/convert_transparent.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 export LC_NUMERIC="en_US.UTF-8"
@@ -9,13 +8,11 @@ for file in light.*.ppm;
 do 
 	convert $file -transparent white ${file:0:11}.png;
 done
-
-img2webp -o ../../../../../docs/sphinx/source/tutorials/figures/level0/lennard-jones-fluid/binary_LJ_fluid_light.webp -q 30 -mixed -d 50 light*.png
+img2webp -o lennard-jones-fluid-avatar-light.webp -q 30 -mixed -d 50 light*.png
 
 # 2) Generate black background movie
 for file in dark.*.ppm; 
 do 
 	convert $file -transparent black ${file:0:10}.png;
 done
-
-img2webp -o ../../../../../docs/sphinx/source/tutorials/figures/level0/lennard-jones-fluid/binary_LJ_fluid_dark.webp -q 30 -mixed -d 50 dark*.png
+img2webp -o lennard-jones-fluid-avatar-dark.webp -q 30 -mixed -d 50 dark*.png
